c70168b7a0dffdb8fdceeb3cf0fbe0df3a9bb6c4
weeaa <github@ryanriviere.com>
2025-01-16 19:14:17 +0100

    update tip account

--- constants.go
+++ constants.go
@@ -2,7 +2,34 @@ package temporal
 
 import (
 	"github.com/gagliardetto/solana-go"
+	"math/rand"
+	"time"
 )
+
+var NOZOMI_TIP_ADDRESSES = []string{
+	"TEMPaMeCRFAS9EKF53Jd6KpHxgL47uWLcpFArU1Fanq",
+	"noz3jAjPiHuBPqiSPkkugaJDkJscPuRhYnSpbi8UvC4",
+	"noz3str9KXfpKknefHji8L1mPgimezaiUyCHYMDv1GE",
+	"noz6uoYCDijhu1V7cutCpwxNiSovEwLdRHPwmgCGDNo",
+	"noz9EPNcT7WH6Sou3sr3GGjHQYVkN3DNirpbvDkv9YJ",
+	"nozc5yT15LazbLTFVZzoNZCwjh3yUtW86LoUyqsBu4L",
+	"nozFrhfnNGoyqwVuwPAW4aaGqempx4PU6g6D9CJMv7Z",
+	"nozievPk7HyK1Rqy1MPJwVQ7qQg2QoJGyP71oeDwbsu",
+	"noznbgwYnBLDHu8wcQVCEw6kDrXkPdKkydGJGNXGvL7",
+	"nozNVWs5N8mgzuD3qigrCG2UoKxZttxzZ85pvAQVrbP",
+	"nozpEGbwx4BcGp6pvEdAh1JoC2CQGZdU6HbNP1v2p6P",
+	"nozrhjhkCr3zXT3BiT4WCodYCUFeQvcdUkM7MqhKqge",
+	"nozrwQtWhEdrA6W8dkbt9gnUaMs52PdAv5byipnadq3",
+	"nozUacTVWub3cL4mJmGCYjKZTnE9RbdY5AP46iQgbPJ",
+	"nozWCyTPppJjRuw2fpzDhhWbW355fzosWSzrrMYB1Qk",
+	"nozWNju6dY353eMkMqURqwQEoM3SFgEKC6psLCSfUne",
+	"nozxNBgWohjR75vdspfxR5H9ceC7XXH99xpxhVGt3Bb",
+}
+
+func PickRandomNozomiTipAddress() string {
+	rand.NewSource(time.Now().UnixNano())
+	return NOZOMI_TIP_ADDRESSES[rand.Intn(len(NOZOMI_TIP_ADDRESSES))]
+}
 
 var NOZOMI_TIP = solana.MustPublicKeyFromBase58("TEMPaMeCRFAS9EKF53Jd6KpHxgL47uWLcpFArU1Fanq")
 var MIN_TIP_AMOUNT uint64 = 1000000
